4e1ce940d992de9a9e5c4ddaff90ffc8fdad7dfa
David <1917116+Syrail@users.noreply.github.com>
2021-04-26 19:06:10 -0400

    Modify Scene Loads to be single

--- Assets/Scripts/MenuDriver.cs
+++ Assets/Scripts/MenuDriver.cs
@@ -7,7 +7,7 @@ public class MenuDriver : MonoBehaviour
 
     public void GameStart()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Assets/Scenes/Syrail_Test.unity");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Assets/Scenes/Syrail_Test.unity", UnityEngine.SceneManagement.LoadSceneMode.Single);
     }
 
     public void GameQuit()
